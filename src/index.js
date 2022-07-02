d45ad9344e266c074d5737d56451d42266cdc856
Eduardo Pacheco <eduardooliveirapacheco131@gmail.com>
2022-07-02 19:28:41 -0300

    Post na rota 'login' concluído

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import chalk from "chalk";
 import joi from "joi";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 dotenv.config();
@@ -24,28 +24,72 @@ const registerSchema = joi.object({
     password: joi.string().required()
 });
 
-server.post("/cadastro", async (req, res) => {
-    const user = req.body;
+const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+});
 
-    const registerValidation = registerSchema.validate(user);
+server.post("/cadastro", async (req, res) => {
+    const newUser = req.body;
+
+    const registerValidation = registerSchema.validate(newUser);
     if(registerValidation.error) {
         return res.status(422).send("Entidade de cadastro não processável");
     }
 
-    const findUser = await db.collection("users").findOne({ name: user.name });
-    const findEmail = await db.collection("users").findOne({ email: user.email });
+    const findUser = await db.collection("users").findOne({ name: newUser.name });
+    const findEmail = await db.collection("users").findOne({ email: newUser.email });
     if(findUser || findEmail) {
         return res.status(409).send("Usuário existente");
     }
 
     try {
-        const passwordHash = bcrypt.hashSync(user.password, 10);
-        await db.collection("users").insertOne({ ...user, password: passwordHash })
+        const passwordHash = bcrypt.hashSync(newUser.password, 10);
+        await db.collection("users").insertOne({ ...newUser, password: passwordHash })
         res.status(201).send("Usuário cadastrado com sucesso");
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }    
+})
+
+server.post("/login", async (req, res) => {
+    const user = req.body
+
+    const loginValidation = loginSchema.validate(user);
+    if(loginValidation.error) {
+        return res.status(422).send("Entidade de login não processável");
+    }
+
+    const findUser = await db.collection("users").findOne({ email: user.email });
+    if(!findUser || !(bcrypt.compareSync(user.password, findUser.password))) {
+        return res.status(401).send("Login não autorizado");
+    }
+    const findToken = await db.collection("sessions").findOne({ idUser: findUser._id });
+    if(!findToken) {
+        const token = uuid();
+        await db.collection("sessions").insertOne({
+            token,
+            idUser: findUser._id
+        })
+        res.send({
+            name: findUser.name,
+            token: token
+        });
+    } else {
+        const token = uuid();
+        await db.collection("sessions").updateOne(
+            { 
+                idUser: findUser._id 
+            }, 
+            { 
+                $set: { token: token }
+            })
+        res.send({
+            name: findUser.name,
+            token: token
+        });
+    }
 })
 
 const PORT = process.env.PORT || 5000;
